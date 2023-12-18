c85cf16db831c7488da4a08791e0afb17dfd4e9b
GBM1986 <gunnar_bjarni@hotmail.com>
2023-12-18 13:46:53 +0100

    test

---
+++ assets/main.js
@@ -0,0 +1,92 @@
+const body = document.body; // Change this line to select the body element
+const context = body.getContext("2d"); // Change this line to get the 2D context of the body
+context.globalAlpha = 0.5;
+
+const cursor = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+let particlesArray = [];
+
+generateParticles(101);
+setSize();
+anim();
+
+addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX;
+  cursor.y = e.clientY;
+});
+
+addEventListener(
+  "touchmove",
+  (e) => {
+    e.preventDefault();
+    cursor.x = e.touches[0].clientX;
+    cursor.y = e.touches[0].clientY;
+  },
+  { passive: false },
+);
+
+addEventListener("resize", () => setSize());
+
+function generateParticles(amount) {
+  for (let i = 0; i < amount; i++) {
+    particlesArray[i] = new Particle(
+      innerWidth / 2,
+      innerHeight / 2,
+      4,
+      generateColor(),
+      0.02,
+    );
+  }
+}
+
+function generateColor() {
+  let hexSet = "0123456789ABCDEF";
+  let finalHexString = "#";
+  for (let i = 0; i < 6; i++) {
+    finalHexString += hexSet[Math.ceil(Math.random() * 15)];
+  }
+  return finalHexString;
+}
+
+function setSize() {
+  body.height = innerHeight; // Change this line to set the body height
+  body.width = innerWidth;   // Change this line to set the body width
+}
+
+function Particle(x, y, particleTrailWidth, strokeColor, rotateSpeed) {
+  this.x = x;
+  this.y = y;
+  this.particleTrailWidth = particleTrailWidth;
+  this.strokeColor = strokeColor;
+  this.theta = Math.random() * Math.PI * 2;
+  this.rotateSpeed = rotateSpeed;
+  this.t = Math.random() * 50;
+
+  this.rotate = () => {
+    const ls = {
+      x: this.x,
+      y: this.y,
+    };
+    this.theta += this.rotateSpeed;
+    this.x = cursor.x + Math.cos(this.theta) * this.t;
+    this.y = cursor.y + Math.sin(this.theta) * this.t;
+    context.beginPath();
+    context.lineWidth = this.particleTrailWidth;
+    context.strokeStyle = this.strokeColor;
+    context.moveTo(ls.x, ls.y);
+    context.lineTo(this.x, this.y);
+    context.stroke();
+  };
+}
+
+function anim() {
+  requestAnimationFrame(anim);
+
+  context.fillStyle = "rgba(0,0,0,0.05)";
+  context.fillRect(0, 0, body.width, body.height); // Change this line to fill the body dimensions
+
+  particlesArray.forEach((particle) => particle.rotate());
+}
---
+++ assets/particles.js
@@ -0,0 +1,110 @@
+class myRect {
+    constructor (ctx) {
+
+        this.ctx = ctx;
+
+        this.updateRect();
+    }
+
+    updateRect() {
+        this.x = Math.random() * 800
+        this.y = Math.random() * 800
+        this.width = Math.random() * 400
+        this.height = Math.random() * 600
+
+        this.drawRect();
+    }
+    drawRect() {
+        this.ctx.fillStyle = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255},${Math.random()})`;
+
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    }
+}
+
+const myCanvas = document.getElementById('myCanvas')
+
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerHeight;
+
+const ctx = myCanvas.getContext('2d');
+
+console.log(ctx)
+
+
+let myArray = [];
+
+
+for (let i = 0; i < 100; i++) {
+    myArray.push(new myRect(ctx));
+}
+
+
+var myTimer = setInterval(change,100)
+
+
+function change(){
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+    myArray.forEach(element => {
+        element.updateRect();
+    });
+
+}
+
+class Controller {
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.particles = [];
+        this.numParticles = 20;
+        this.createParticles();
+    }
+
+    createParticles() {
+        for (let index = 0; index < this.numParticles; index++) {
+            this.particles.push(new Particle(this));
+
+        }
+    }
+
+    handleParticle() {
+        this.particles.forEach(particle => {
+            particle.update();
+            particle.draw(this.ctx);
+        })
+    }
+}
+
+class Particle {
+    constructor (controller) {
+        this.controller = controller;
+        this.radius = Math.random() * 10;
+
+        this.x = this.radius + Math.random() * (this.controller.width - this.radius * 2);
+        this.y = this.radius + Math.random() * (this.controller.height - this.radius * 2);
+
+
+
+    }
+
+    update() {
+        this.x++;
+
+    }
+
+    draw(ctx) {
+
+        ctx.fillStyle = `hsl(${300},100%,50%)`
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+
+}
+
+
+
